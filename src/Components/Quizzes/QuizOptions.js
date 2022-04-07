e48057dd80aa87900c72d8a1bb6c56d0ade2d299
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-04-07 10:13:09 -0700

    options component bug fixes and quiz data corrected

--- src/Components/Quizzes/QuizOptions.js
+++ src/Components/Quizzes/QuizOptions.js
@@ -2,19 +2,35 @@ import { optionLabels } from "../../Model/Helpers";
 import { useState, useEffect } from "react";
 
 function QuizOptions(props) {
-  const [currentOption, setCurrentOption] = useState(-1);
-  const { option, index } = props;
+  //   const [currentOption, setCurrentOption] = useState(-1);
+  const { options, currentOption, setCurrentOption } = props;
 
   useEffect(() => {
     setCurrentOption(-1);
   }, []);
   return (
-    <div
-      onClick={() => setCurrentOption(index)}
-      className={index === currentOption ? "option-row-selected" : "option-row"}
-    >
-      <label>{optionLabels[index]}</label>
-      <li className="option-label">{option}</li>
+    // <div
+    //   onClick={() => setCurrentOption(index)}
+    //   className={index === currentOption ? "option-row-selected" : "option-row"}
+    // >
+    //   <label>{optionLabels[index]}</label>
+    //   <li className="option-label">{option}</li>
+    // </div>
+
+    <div>
+      {options.map((e, index) => {
+        return (
+          <div
+            onClick={() => setCurrentOption(index)}
+            className={
+              index === currentOption ? "option-row-selected" : "option-row"
+            }
+          >
+            <label>{optionLabels[index]}</label>
+            <li className="option-label">{e}</li>
+          </div>
+        );
+      })}
     </div>
   );
 }
