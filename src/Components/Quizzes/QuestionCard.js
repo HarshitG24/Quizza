6a337e72fcf330f9504d9bd5bcba4233207e063f
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-04-07 10:13:09 -0700

    options component bug fixes and quiz data corrected

--- src/Components/Quizzes/QuestionCard.js
+++ src/Components/Quizzes/QuestionCard.js
@@ -4,9 +4,9 @@ import { useState, useEffect } from "react";
 import QuizOptions from "./QuizOptions";
 
 function QuestionCard(props) {
-  const { question, index } = props;
+  const { question, index, currentOption, setCurrentOption } = props;
 
-  const [currentOption, setCurrentOption] = useState(-1);
+  // const [currentOption, setCurrentOption] = useState(-1);
   const [optionsArray, setOptionsArray] = useState([]);
 
   // function shuffleArray(array) {
@@ -28,25 +28,13 @@ function QuestionCard(props) {
         <p>Question {index + 1}</p>
         <p>{question.question}</p>
         <ul className="quiz-options">
-          {question.allOptions.map((option, index) => {
-            return (
-              <div
-                onClick={() => setCurrentOption(index)}
-                className={
-                  index === currentOption ? "option-row-selected" : "option-row"
-                }
-              >
-                <label>{optionLabels[index]}</label>
-                <li className="option-label">{option}</li>
-              </div>
-              // <QuizOptions
-              //   option={option}
-              //   index={index}
-              //   // currentOption={currentOption}
-              //   // setCurrentOption={setCurrentOption}
-              // />
-            );
-          })}
+          <QuizOptions
+            options={question.allOptions}
+            currentOption={currentOption}
+            setCurrentOption={setCurrentOption}
+            // currentOption={currentOption}
+            // setCurrentOption={setCurrentOption}
+          />
         </ul>
       </div>
     </div>
