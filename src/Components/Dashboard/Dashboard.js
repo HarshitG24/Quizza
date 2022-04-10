bd1f1cce471491a22f738858104716f65524c17d
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-04-09 18:49:51 -0700

    made dashboard

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,16 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getData } from "../../Model/Mininmongo";
+import "./css/Dashboard.css";
+import QuizCard from "./QuizCard";
 
 function Dashboard(props) {
+  const [quizesTaken, setQuizesTaken] = useState([]);
   useEffect(() => {
     getData("quiz_score", "quiz", (fetchedResult) => {
       console.log("fetchedResult for dashboard", fetchedResult);
+      setQuizesTaken(fetchedResult);
     });
   }, []);
 
   return (
-    <div>
-      <p>Dashboard</p>
+    <div className="dashboard">
+      {quizesTaken.length > 0 ? (
+        <h3>Following are the quizes taken so far</h3>
+      ) : null}
+
+      {quizesTaken.length > 0 ? (
+        quizesTaken.map((q, index) => {
+          return <QuizCard key={index} quiz={q} />;
+        })
+      ) : (
+        <div className="no-test">
+          <p className="no-test-text">No Tests taken</p>
+        </div>
+      )}
     </div>
   );
 }
