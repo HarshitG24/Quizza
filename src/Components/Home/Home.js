87935d5a5397bc7fde67f21ccc99828825f534d1
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-04-13 10:42:34 -0700

    latest working code

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,25 @@
+import "./css/Home.css";
+
+//https://cdni.iconscout.com/illustration/premium/thumb/young-people-study-prepare-for-exam-get-knowledge-2706162-2260911.png
+//cdni.iconscout.com/illustration/premium/thumb/knowledge-ladder-4911970-4088928.png
 function Home() {
   return (
-    <div>
-      <label>Home</label>
+    <div className="home">
+      <header>
+        <div className="img-position">
+          <img
+            src="https://cdni.iconscout.com/illustration/premium/thumb/young-people-study-prepare-for-exam-get-knowledge-2706162-2260911.png"
+            alt="abc"
+            className="home-img"
+          />
+        </div>
+        <div className="img-position">
+          <p className="home-caption">
+            The beautiful thing about learning is nobody can take it away from
+            you
+          </p>
+        </div>
+      </header>
     </div>
   );
 }
--- src/Components/Home/Home2.js
+++
@@ -1,9 +0,0 @@
-function Home2() {
-  return (
-    <div>
-      <h1>home 2</h1>
-    </div>
-  );
-}
-
-export default Home2;
