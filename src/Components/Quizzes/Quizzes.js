f5967319a456674b2cf8dafa3a3dd7dfe767d0a7
Harshit Gajjar <hrstgajjar3@gmail.com>
2022-04-16 17:13:42 -0700

    jsdoc code added

--- src/Components/Quizzes/Quizzes.js
+++ src/Components/Quizzes/Quizzes.js
@@ -1,6 +1,14 @@
 import { availableCategories } from "../../Quiz Questions/Data";
 import QuizCategoryCard from "./QuizCategoryCard";
 
+/**
+ * Component for showing the UI of the cards which shows category names.
+ * @component
+ * @param {Object} Props - An object.
+ * @param {String} props.selectedCategory - Variable which contains the name of the category selected by the user
+ * @param {Function} props.setCategory - Function which updates the variable selectedCategory with the category selected by the user.
+ */
+
 function Quizzes(props) {
   const { setCategory, selectedCategory } = props;
   return (
